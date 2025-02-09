e57c9d7b294cff23191eb6b355ec61d311d77a64
Erica Marigold <hi@devcomp.xyz>
2025-02-09 11:05:58 +0000

    chore: include nix configs for devenv

---
+++ dev.nix
@@ -0,0 +1,33 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.curl
+    pkgs.git
+    pkgs.lune
+    (let version = "6.0-28"; in pkgs.stdenv.mkDerivation {
+      name = "unzip-${version}";
+      src = pkgs.fetchurl {
+        url = "http://ftp.debian.org/debian/pool/main/u/unzip/unzip_${version}_amd64.deb";
+        sha256 = "13h6cm4708p5zczi7fc4qbdlwl1nhnhrk0ncwv9chb1q6jf55ndk";
+      };
+
+      buildInputs = [ pkgs.binutils pkgs.gnutar pkgs.bzip2 pkgs.pkg-config ];
+
+      unpackPhase = ''
+        mkdir -p tmp/
+        ar x $src
+        mv data.tar.xz tmp/
+      '';
+
+      installPhase = ''
+        mkdir -p $out/bin $out/lib
+        tar -xf ./tmp/data.tar.xz -C $out
+        mv $out/usr/bin/unzip $out/bin/
+        cp $(pkg-config --variable=libdir bzip2)/libbz2.so.1.0.8 $out/lib/libbz2.so.1.0
+        rm -rf $out/usr tmp/
+      '';
+    })
+  ];
+}
+
---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "luau-unzip development environment";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      devpkgs = import ./dev.nix {
+        inherit pkgs;
+      };
+    in {
+      packages.${system}.devShell = devpkgs;
+      defaultPackage.${system} = devpkgs;
+    };
+}
+
